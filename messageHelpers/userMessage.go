5362e337e9e4f33708395d7fe2d0ebcf34ec8b8e
Matt Townsend <mt76@gmx.com>
2025-03-04 13:17:32 +0000

    Add Source field and validation method to UserMessage type

--- messageHelpers/userMessage.go
+++ messageHelpers/userMessage.go
@@ -1,19 +1,42 @@
 package messageHelpers
 
+import (
+	"fmt"
+	"log"
+)
+
 type UserMessage struct {
 	Key     string `json:"Key"`
 	Code    string `json:"Code"`
 	Payload any    `json:"Payload"`
+	Source  string `json:"Source"`
 }
 
-func (m *UserMessage) Request(key string, code string) UserMessage {
+func (m *UserMessage) Request(key, code, source string) UserMessage {
 	message := UserMessage{}
 	message.Key = key
 	message.Code = code
+	message.Source = source
 	return message
 }
 
 func (m *UserMessage) Response(payload any) UserMessage {
 	m.Payload = payload
 	return *m
+}
+
+func (m *UserMessage) Validate(log *log.Logger) error {
+	if m.Key == "" {
+		log.Println("user Key is required")
+		return fmt.Errorf("user Key is required")
+	}
+	if m.Code == "" {
+		log.Println("user Code is required")
+		return fmt.Errorf("user Code is required")
+	}
+	if m.Source == "" {
+		log.Println("user Source is required")
+		return fmt.Errorf("user Source is required")
+	}
+	return nil
 }
