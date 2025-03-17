c640d4d5ae7e6e97c8f8f2982bb8451d1ce346af
Matt Townsend <mt76@gmx.com>
2025-03-17 18:48:58 +0000

    Add host availability check with platform-specific ping implementation

---
+++ netHandler/netHandler.go
@@ -0,0 +1,62 @@
+package netHandler
+
+import (
+	"os/exec"
+	"strings"
+
+	"github.com/mt1976/frantic-core/application"
+	"github.com/mt1976/frantic-core/logHandler"
+)
+
+var sucess = "Host %v is reachable"
+var failure = "Host %v is not reachable"
+
+func CheckHostAvailability(addr string) (bool, error) {
+	//
+	// This is a simple ping function that will return true if the host is up and false if it is down
+	//
+	// addr: string: The IP address of the host to ping
+	// windows: bool: If the host is running windows, then we need to use a different method to ping it
+	logHandler.ServiceLogger.Printf("Pinging %v", addr)
+	var out []byte
+	var err error
+
+	if application.IsRunningOnWindows() {
+		logHandler.ServiceLogger.Printf("Running Windows Ping - [ping %v -n 5 -w 3000]", addr)
+		out, err = exec.Command("ping", addr, "-n", "5", "-w", "3000").Output()
+	} else {
+		logHandler.ServiceLogger.Printf("Running Linux Ping - [ping %v -c 5 -i 3 -W 10]", addr)
+		out, err = exec.Command("ping", addr, "-c 5", "-i 3", "-W 10").Output()
+	}
+	if err != nil {
+		if strings.Contains(err.Error(), "exit status 68") {
+			logHandler.WarningLogger.Printf(failure, addr)
+			return false, nil
+		}
+	}
+
+	if isHostReachable(out) {
+		logHandler.ServiceLogger.Printf(sucess, addr)
+		return true, nil
+	}
+
+	logHandler.WarningLogger.Printf(failure, addr)
+	return false, nil
+}
+
+func isHostReachable(out []byte) bool {
+
+	switch {
+	case strings.Contains(string(out), "Destination Host Unreachable"):
+		return false
+	case strings.Contains(string(out), "Request timed out"):
+		return false
+	case strings.Contains(string(out), "100% packet loss"):
+		return false
+	case strings.Contains(string(out), "Request timeout"):
+		return false
+	case strings.Contains(string(out), "cannot resolve"):
+		return false
+	}
+	return true
+}
