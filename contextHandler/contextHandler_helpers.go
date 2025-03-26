843b5a56dc7fd763d3fdd7dfa510bb8c82c177bf
Matt Townsend <mt76@gmx.com>
2025-03-26 10:49:46 +0000

    Refactor Debug function to use key names for improved clarity in session logging

--- contextHandler/contextHandler_helpers.go
+++ contextHandler/contextHandler_helpers.go
@@ -16,16 +16,16 @@ func Debug(c context.Context, name string) {
 
 	logHandler.TraceLogger.Printf("[%v] Session ID: [%v]=[%v]", strings.ToUpper(name), sessionIDKey.name, GetSession_ID(c))
 
-	logHandler.TraceLogger.Printf("[%v] Session U Code: [%v]=[%v]", strings.ToUpper(name), userCodeKey, GetSession_UserCode(c))
-	logHandler.TraceLogger.Printf("[%v] Session U Key: [%v]=[%v]", strings.ToUpper(name), userKeyKey, GetSession_UserKey(c))
-	logHandler.TraceLogger.Printf("[%v] Session U Role: [%v]=[%v]", strings.ToUpper(name), userRoleKey, GetSession_UserRole(c))
+	logHandler.TraceLogger.Printf("[%v] Session U Code: [%v]=[%v]", strings.ToUpper(name), userCodeKey.name, GetSession_UserCode(c))
+	logHandler.TraceLogger.Printf("[%v] Session U Key: [%v]=[%v]", strings.ToUpper(name), userKeyKey.name, GetSession_UserKey(c))
+	logHandler.TraceLogger.Printf("[%v] Session U Role: [%v]=[%v]", strings.ToUpper(name), userRoleKey.name, GetSession_UserRole(c))
 
-	logHandler.TraceLogger.Printf("[%v] Session Expiry: [%v]=[%v]", strings.ToUpper(name), expiryPeriodKey, GetSession_Expiry(c))
-	logHandler.TraceLogger.Printf("[%v] Session Theme: [%v]=[%v]", strings.ToUpper(name), themeKey, GetSession_Theme(c))
-	logHandler.TraceLogger.Printf("[%v] Session Locale: [%v]=[%v]", strings.ToUpper(name), localeKey, GetSession_Locale(c))
-	logHandler.TraceLogger.Printf("[%v] Session Timezone: [%v]=[%v]", strings.ToUpper(name), timezoneKey, GetSession_Timezone(c))
+	logHandler.TraceLogger.Printf("[%v] Session Expiry: [%v]=[%v]", strings.ToUpper(name), expiryPeriodKey.name, GetSession_Expiry(c))
+	logHandler.TraceLogger.Printf("[%v] Session Theme: [%v]=[%v]", strings.ToUpper(name), themeKey.name, GetSession_Theme(c))
+	logHandler.TraceLogger.Printf("[%v] Session Locale: [%v]=[%v]", strings.ToUpper(name), localeKey.name, GetSession_Locale(c))
+	logHandler.TraceLogger.Printf("[%v] Session Timezone: [%v]=[%v]", strings.ToUpper(name), timezoneKey.name, GetSession_Timezone(c))
 	logHandler.TraceLogger.Printf("[%v] Session TOKEN", name)
-	logHandler.TraceLogger.Printf("[%v] Session Token: [%v]=[%+v]", strings.ToUpper(name), tokenKey, GetSession_Token(c))
+	logHandler.TraceLogger.Printf("[%v] Session Token: [%v]=[%+v]", strings.ToUpper(name), tokenKey.name, GetSession_Token(c))
 	logHandler.TraceLogger.Printf("[%v] DEBUG ENDS", name)
 	logHandler.TraceLogger.Printf("[%v] DEBUG ENDS", name)
 }
